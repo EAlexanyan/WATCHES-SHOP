b89c015b3b40804de4d027b6f1d0c33bcebb9cca
EAlexanyan <alexanyan.e@mail.ru>
2023-01-02 20:21:27 +0400

    NavBar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import NavBar from './Components/NavBar/NavBar';
+import './App.css';
+// import { memo } from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+          <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div className='navBar'>
+        <div className='navLogo'>
+            <a href='#'>
+               <h2>WATCHES</h2>
+               <h3>ONLINE STORE</h3>
+            </a>
+        </div>
+
+        <div className='navInput'>
+            <form>
+                <input type='text' placeholder='Search store' />
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </form>
+        </div>
+
+        <div className='navSign'>
+            <a href='#'><i class="fa-solid fa-bag-shopping"></i></a>
+             <a href='#'><i class="fa-solid fa-right-to-bracket"></i></a>              
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
